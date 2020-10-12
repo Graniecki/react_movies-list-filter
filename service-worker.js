c986c90e230e2b5ec181ecd58103b7757114e62b
Ihor Hranetskyi <graniecki20@gmail.com>
2020-10-12 19:28:30 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.520d4294590a353bae9dfdf7775894f7.js"
+  "./precache-manifest.d28017cfd9a7ee2fa7afad486f46b05f.js"
 );
 
 self.addEventListener('message', (event) => {
